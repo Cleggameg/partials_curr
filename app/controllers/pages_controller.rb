c8264e6a23824aa1dc21e6cd6958d0668adcca8f
Paul Clegg <paul_clegg@kleinschmidt.com>
2016-07-12 09:24:25 -0500

    added content pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  before_action :set_page, only: [:show, :edit, :update, :destroy]
+
 
   # GET /pages
   # GET /pages.json
@@ -10,6 +10,7 @@ class PagesController < ApplicationController
   # GET /pages/1
   # GET /pages/1.json
   def show
+    render template: "pages/#{params[:page]}"
   end
 
   # GET /pages/new
@@ -63,9 +64,7 @@ class PagesController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_page
-      @page = Page.find(params[:id])
-    end
+
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def page_params
